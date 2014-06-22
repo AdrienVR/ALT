3190ed78e4934ed679ddde6bbd8b405baa906318
AdrienVR <a1vernot@enib.fr>
2014-06-22 12:44:35 +0200

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import os
 import sys
 
 import PySide
-import PySide.uic
 from cx_Freeze import setup, Executable
 
 
@@ -20,11 +19,10 @@ importationCode=[]
 dependances = ["cours/","qcm/","qcmrm/","toeic/","users/"]
 
 #QWEB
-includefiles = ["res_rc.py","resUsr_rc.py","resPref_rc.py","resLevel_rc.py",(os.path.join(os.path.dirname(PyQt4.uic.__file__),
-"widget-plugins"), "PyQt4.uic.widget-plugins")]+importationCode+dependances
+includefiles = ["res_rc.py","resUsr_rc.py","resPref_rc.py","resLevel_rc.py"]+importationCode+dependances
 #QWEB!
 
-includes = ["PyQt4.QtNetwork"]
+includes = []
 excludes = []
 packages = ["encodings",
             "OpenGL",
@@ -33,7 +31,7 @@ packages = ["encodings",
 #
 setup(
     name = "Learning",
-    version = "0.2",
+    version = "0.3",
     description = "Learning",
     executables = [Executable("Ui_main.py", base = base)],
     options = {'build_exe': {'excludes':excludes,"compressed":True,
